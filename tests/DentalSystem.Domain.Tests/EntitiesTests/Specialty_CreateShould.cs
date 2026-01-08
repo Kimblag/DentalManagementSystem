670380da7bd57d312a46532533b3db398e34d997
Kimblag <kimberly.blandong@gmail.com>
2026-01-08 09:43:17 -0300

    test(domain): add unit tests for Specialty and Treatment aggregates

---
+++ tests/DentalSystem.Domain.Tests/EntitiesTests/Specialty_CreateShould.cs
@@ -0,0 +1,121 @@
+﻿using Xunit;
+using DentalSystem.Domain.Entities;
+using DentalSystem.Domain.Enums;
+using DentalSystem.Domain.Exceptions;
+
+namespace DentalSystem.Domain.Tests.EntitiesTests
+{
+    public class Specialty_CreateShould
+    {
+        // Happy path
+        [Fact]
+        public void Create_WhenDataIsValid_ShouldCreateSpecialtySuccessfully()
+        {
+            // Follow A-A-A: Arrange - Act - Assert
+
+            // Arrange
+            string treatmentName = "Braces";
+            string treatmentDescription = "Metal or ceramic devices to straighten teeth.";
+            decimal treatmentBaseCost = 10.0m;
+
+            var treatments = new List<Treatment> {
+
+                new Treatment(treatmentName, treatmentBaseCost, treatmentDescription)
+            };
+
+            string specialtyName = "Orthodontics";
+            string specialtyDescription = "Focuses on correcting teeth and jaw alignment issues.";
+
+
+            // Act: Invoke the entity that is being tested
+            Specialty specialty = new Specialty(specialtyName, treatments, specialtyDescription);
+
+
+            // Assert
+
+            // Validate Aggregate: Specialty
+            Assert.Equal(specialtyName, specialty.Name);
+            Assert.Equal(specialtyDescription, specialty.Description);
+            Assert.Equal(EntityStatus.Active, specialty.Status);
+            Assert.Single(specialty.Treatments);
+
+            // Validate Aggregate member: Treatment
+            Treatment createdTreatment = specialty.Treatments.First();
+            Assert.Equal(treatmentName, createdTreatment.Name);
+            Assert.Equal(treatmentDescription, createdTreatment.Description);
+            Assert.Equal(treatmentBaseCost, createdTreatment.BaseCost);
+            Assert.Equal(EntityStatus.Active, createdTreatment.Status);
+        }
+
+
+        // multitest: we can pass multiple cases
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData("   ")]
+        [InlineData("or")]
+        public void Create_WhenNameIsInvalid_ShouldThrowInvalidNameException(string? invalidName)
+        {
+            // Arrange
+            string treatmentName = "Braces";
+            string treatmentDescription = "Metal or ceramic devices to straighten teeth.";
+            decimal treatmentBaseCost = 10.0m;
+
+            var treatments = new List<Treatment> {
+
+                new Treatment(treatmentName, treatmentBaseCost, treatmentDescription)
+            };
+
+            string specialtyDescription = "Focuses on correcting teeth and jaw alignment issues.";
+
+            // Act and Assert
+            Assert.Throws<InvalidSpecialtyNameException>(() =>
+            {
+                new Specialty(invalidName!, treatments, specialtyDescription);
+            });
+           
+        }
+
+
+        [Fact]
+        public void Create_WhenEmptyTreatmentList_ShouldThrowEmptyTreatmentListException()
+        {
+            // Arrange
+            var invalidTreatments = new List<Treatment>();
+
+            string specialtyName = "Orthodontics";
+            string specialtyDescription = "Focuses on correcting teeth and jaw alignment issues.";
+
+            // Act and Assert
+            Assert.Throws<EmptyTreatmentListException>(() =>
+            {
+                new Specialty(specialtyName, invalidTreatments, specialtyDescription);
+            });
+        }
+        
+
+        [Fact]
+        public void Create_WhenTreatmentNamesAreDuplicatedInList_ShouldThrowDuplicateTreatmentNameException()
+        {
+            // Arrange
+            string duplicateName = "Braces";
+
+            var duplicateTreatments = new List<Treatment> {
+
+                new Treatment(duplicateName, 10.0m, "Instance 1"),
+                new Treatment(duplicateName, 20.0m, "Instance 2"),
+            };
+
+            string specialtyName = "Orthodontics";
+            string specialtyDescription = "Focuses on correcting teeth and jaw alignment issues.";
+
+            // Act and Assert
+            Assert.Throws<DuplicateTreatmentNameException>(() =>
+            {
+                new Specialty(specialtyName, duplicateTreatments, specialtyDescription);
+            });
+        }
+
+
+    }
+}
---
+++ tests/DentalSystem.Domain.Tests/EntitiesTests/Treatment_CreateShould.cs
@@ -0,0 +1,67 @@
+﻿using Xunit;
+
+using DentalSystem.Domain.Entities;
+using DentalSystem.Domain.Enums;
+using DentalSystem.Domain.Exceptions;
+
+namespace DentalSystem.Domain.Tests.EntitiesTests
+{
+    public class Treatment_CreateShould
+    {
+
+        [Fact]
+        public void Create_WhenDataIsValid_ShouldCreateTreatmentSuccessfully()
+        {
+            // Arrange
+            string treatmentName = "Braces";
+            string treatmentDescription = "Metal or ceramic devices to straighten teeth.";
+            decimal treatmentBaseCost = 10.0m;
+
+            // Act: Invoke the entity that is being tested
+            Treatment treatment = new Treatment(treatmentName, treatmentBaseCost, treatmentDescription);
+
+            // Assert
+            // Validate Aggregate member: Treatment
+            Assert.Equal(treatmentName, treatment.Name);
+            Assert.Equal(treatmentDescription, treatment.Description);
+            Assert.Equal(treatmentBaseCost, treatment.BaseCost);
+            Assert.Equal(EntityStatus.Active, treatment.Status);
+        }
+
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData("   ")]
+        [InlineData("or")]
+        public void Create_WhenTreatmentNameIsInvalid_ShouldThrowInvalidNameException(string? invalidName)
+        {
+            // Arrange
+            string treatmentDescription = "Metal or ceramic devices to straighten teeth.";
+            decimal treatmentBaseCost = 10.0m;
+
+            // Act and Assert
+            Assert.Throws<InvalidTreatmentNameException>(() =>
+            {
+                new Treatment(invalidName!, treatmentBaseCost, treatmentDescription);
+            });
+        }
+
+
+        [Fact]
+        public void Create_WhenTreatmentBaseCostIsInvalid_ShouldThrowInvalidTreatmentCostException()
+        {
+            // Arrange
+            string treatmentName = "Braces";
+            string treatmentDescription = "Metal or ceramic devices to straighten teeth.";
+            decimal invalidTreatmentBaseCost = -100;
+
+            // Act and Assert
+            Assert.Throws<InvalidTreatmentCostException>(() =>
+            {
+                new Treatment(treatmentName, invalidTreatmentBaseCost, treatmentDescription);
+            });
+        }
+
+    }
+}
