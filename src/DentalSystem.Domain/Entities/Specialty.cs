8df0303e39d798d65dc14edb60bb9a3a6e5604c4
Kimblag <kimberly.blandong@gmail.com>
2026-01-09 17:57:52 -0300

    feat(domain): implement Specialty and Treatment entities with DDD invariants

--- src/DentalSystem.Domain/Entities/Specialty.cs
+++ src/DentalSystem.Domain/Entities/Specialty.cs
@@ -1,17 +1,18 @@
-﻿
-
-using DentalSystem.Domain.Enums;
-using DentalSystem.Domain.Exceptions;
+﻿using DentalSystem.Domain.Enums;
+using DentalSystem.Domain.Exceptions.Specialties;
 using System.Text.RegularExpressions;
-using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace DentalSystem.Domain.Entities
 {
     // partial because we need to tell .NET that we need to use a generated regex code
     public partial class Specialty
     {
+        // Domain identity
+        public Guid SpecialtyId { get; private set; }
+
+        // EF persistence Identity
         public int Id { get; private set; }
-        public string Name { get; private set; }
+        public string Name { get; private set; } = null!;
         public string Description { get; private set; } = string.Empty;
         public EntityStatus Status { get; private set; }
         private readonly List<Treatment> _treatments = new();
@@ -23,10 +24,10 @@ namespace DentalSystem.Domain.Entities
         // while allowing them to iterate and read the data.
         public IReadOnlyCollection<Treatment> Treatments => _treatments.AsReadOnly();
 
-        [GeneratedRegex("^[a-zA-Z0-9áéíóúñÁÉÍÓÚÑ][a-zA-Z0-9áéíóúñÁÉÍÓÚÑ\\s-]{1,98}[a-zA-Z0-9áéíóúñÁÉÍÓÚÑ]$")]
+        [GeneratedRegex("^[a-zA-Z0-9áéíóúñÁÉÍÓÚÑ][a-zA-Z0-9áéíóúñÁÉÍÓÚÑ\\s-]{3,100}[a-zA-Z0-9áéíóúñÁÉÍÓÚÑ]$")]
         private static partial Regex NamePattern();
 
-        [GeneratedRegex("^[\\w\\sáéíóúñÁÉÍÓÚÑ\\.,;:\\-\\(\\)¿?¡!]{0,500}$")]
+        [GeneratedRegex("^[\\w\\sáéíóúñÁÉÍÓÚÑ\\.,;:\\-\\(\\)¿?¡!]{3,500}$")]
         private static partial Regex DescriptionPattern();
 
 
@@ -41,10 +42,14 @@ namespace DentalSystem.Domain.Entities
             // not null names and match pattern
             if (string.IsNullOrWhiteSpace(name))
             {
-                throw new InvalidSpecialtyNameException();
-            } else if (!NamePattern().IsMatch(name))
+                throw new InvalidSpecialtyNameException("The specialty name cannot be empty.");
+            }
+
+            name = name.Trim();
+
+            if (!NamePattern().IsMatch(name))
             {
-                throw new InvalidSpecialtyNameException();
+                throw new InvalidSpecialtyNameException("The specialty name contains invalid characters or has an invalid length.");
             }
 
             // Validate treatment list is not empty
@@ -70,14 +75,155 @@ namespace DentalSystem.Domain.Entities
                 // hashset.Add would return false if there is a duplicate
                 if (!seenNames.Add(treatment.Name))
                 {
-                    throw new DuplicateTreatmentNameException();
+                    throw new DuplicateTreatmentNameException($"The treatment '{treatment.Name}' is duplicated.");
                 }
             }
 
             // Create instance
+            SpecialtyId = Guid.NewGuid();
             Name = name;
             Description = description ?? string.Empty;
             _treatments.AddRange(treatments);
+            Status = EntityStatus.Active;
+        }
+
+
+        public void AddTreatment(Treatment newTreatment)
+        {
+            // if treatment is null
+            ArgumentNullException.ThrowIfNull(newTreatment);
+
+            // specialty is not active
+
+            if (Status != EntityStatus.Active)
+            {
+                throw new InvalidSpecialtyStateException();
+            }
+
+            // Name already exists
+            if (_treatments.Any(t => t.Name.Equals(newTreatment.Name, StringComparison.OrdinalIgnoreCase)))
+            {
+                throw new DuplicateTreatmentNameException();
+            }
+
+
+            _treatments.Add(newTreatment);
+        }
+
+
+        public void RemoveTreatment(Guid treatmentId)
+        {
+            // When specialty is not active
+            if (Status != EntityStatus.Active)
+            {
+                throw new InvalidSpecialtyStateException();
+            }
+
+            // When treatment is not found
+            Treatment foundTreatment = _treatments
+               .FirstOrDefault(t => t.TreatmentId == treatmentId)
+               ?? throw new TreatmentNotFoundException(
+                   $"Treatment with ID {treatmentId} was not found in this specialty.");
+
+            // When treatment is already inactive
+            if (foundTreatment.Status != EntityStatus.Active)
+            {
+                throw new TreatmentAlreadyInactiveException();
+            }
+
+            // When is the last active treatment
+            if (_treatments.Count(t => t.Status == EntityStatus.Active) == 1)
+            {
+                throw new MinimumSpecialtyTreatmentsException("A specialty can't have less than one treatment.");
+            }
+
+            // remove
+            foundTreatment.Deactivate();
+        }
+
+
+        public void UpdateDescription(string? description = null)
+        {
+            // Specialty is not active
+            if (Status != EntityStatus.Active)
+            {
+                throw new InvalidSpecialtyStateException(); 
+            }
+
+            if (!string.IsNullOrEmpty(description))
+            {
+                // description does not match pattern
+                if (!DescriptionPattern().IsMatch(description))
+                {
+                    throw new InvalidSpecialtyDescriptionException(); 
+                }
+            }
+
+            // change or clear
+            Description = !string.IsNullOrEmpty(description) ? description : string.Empty;
+        }
+
+
+        public void UpdateTreatmentDetails(string? treatmentName = null, 
+            decimal? treatmentBaseCost = 0.0m, string? treatmentDescription = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Deactivate()
+        {
+            // Check current status
+            if (Status == EntityStatus.Inactive)
+            {
+                throw new InvalidStatusTransitionException();
+            }
+
+            // Set children to inactive
+            _treatments
+                .ForEach(t => t.Deactivate());
+            Status = EntityStatus.Inactive;
+        }
+
+
+        public void CorrectName(string correctedName)
+        {
+            // If specialty is inactive
+            if (Status == EntityStatus.Inactive)
+                throw new InvalidSpecialtyStateException();
+
+            // If is null or invalid
+            if (string.IsNullOrWhiteSpace(correctedName)) 
+                throw new InvalidSpecialtyNameException();
+
+            correctedName = correctedName.Trim();
+
+            // is a correct format?
+            if (!NamePattern().IsMatch(correctedName))
+                throw new InvalidSpecialtyNameException();
+
+            // is identical
+            if (string.Equals(Name, correctedName, StringComparison.OrdinalIgnoreCase))
+                return;
+
+            // Apply correction 
+            Name = correctedName;
+        }
+
+
+        public void Reactivate()
+        {
+            // Is already active
+            if (Status == EntityStatus.Active)
+            {
+                throw new InvalidStatusTransitionException("The specialty is already active.");
+            }
+
+            // reactivate treatments
+            foreach (var treatment in _treatments)
+            {
+                treatment.Reactivate();
+            }
+
             Status = EntityStatus.Active;
         }
     }
--- src/DentalSystem.Domain/Entities/Treatment.cs
+++ src/DentalSystem.Domain/Entities/Treatment.cs
@@ -1,22 +1,26 @@
 ﻿using DentalSystem.Domain.Enums;
-using DentalSystem.Domain.Exceptions;
+using DentalSystem.Domain.Exceptions.Specialties;
 using System.Text.RegularExpressions;
 
 namespace DentalSystem.Domain.Entities
 {
     public partial class Treatment
     {
+        // Domain identity
+        public Guid TreatmentId { get; private set; }
+
+        // EF persistence Identity
         public int Id { get; private set; }
-        public string Name { get; private set; }
+        public string Name { get; private set; } = null!;
         public string Description { get; private set; } = string.Empty;
         public decimal BaseCost { get; private set; } = 0;
         public EntityStatus Status { get; private set; }
 
 
-        [GeneratedRegex("^[a-zA-Z0-9áéíóúñÁÉÍÓÚÑ][a-zA-Z0-9áéíóúñÁÉÍÓÚÑ\\s-]{1,98}[a-zA-Z0-9áéíóúñÁÉÍÓÚÑ]$")]
+        [GeneratedRegex("^[a-zA-Z0-9áéíóúñÁÉÍÓÚÑ][a-zA-Z0-9áéíóúñÁÉÍÓÚÑ\\s-]{3,100}[a-zA-Z0-9áéíóúñÁÉÍÓÚÑ]$")]
         private static partial Regex NamePattern();
 
-        [GeneratedRegex("^[\\w\\sáéíóúñÁÉÍÓÚÑ\\.,;:\\-\\(\\)¿?¡!]{0,500}$")]
+        [GeneratedRegex("^[\\w\\sáéíóúñÁÉÍÓÚÑ\\.,;:\\-\\(\\)¿?¡!]{3,500}$")]
         private static partial Regex DescriptionPattern();
 
         private Treatment()
@@ -31,7 +35,8 @@ namespace DentalSystem.Domain.Entities
             {
                 throw new InvalidTreatmentNameException();
             }
-            else if (!NamePattern().IsMatch(name))
+            name = name.Trim();
+            if (!NamePattern().IsMatch(name))
             {
                 throw new InvalidTreatmentNameException();
             }
@@ -53,10 +58,78 @@ namespace DentalSystem.Domain.Entities
                 }
             }
 
+            TreatmentId = Guid.NewGuid();
             Name = name;
             Description = description ?? string.Empty;
             BaseCost = baseCost;
             Status = EntityStatus.Active;
         }
+
+        internal void Deactivate()
+        {
+            if (Status == EntityStatus.Inactive)
+                throw new TreatmentAlreadyInactiveException();
+
+            Status = EntityStatus.Inactive;
+        }
+
+
+        internal void Reactivate()
+        {
+            if (Status == EntityStatus.Active)
+            {
+                throw new TreatmentAlreadyActiveException();
+            }
+            Status = EntityStatus.Active;
+        }
+
+        internal void CorrectName(string correctedName)
+        {
+            // is inactive
+            if (Status == EntityStatus.Inactive)
+            {
+                throw new InvalidTreatmentStateException();
+            }
+
+            if (string.IsNullOrWhiteSpace(correctedName))
+            {
+                throw new InvalidTreatmentNameException();
+            }
+            correctedName = correctedName.Trim();
+
+            // invalid name
+            if (!NamePattern().IsMatch(correctedName))
+            {
+                throw new InvalidTreatmentNameException();
+            }
+
+            // is identical
+            if (string.Equals(Name, correctedName, StringComparison.OrdinalIgnoreCase))
+            {
+                return;
+            }
+
+            // correct name
+            Name = correctedName;
+        }
+
+        internal void UpdateDetails(decimal? newBaseCost = null, string? newDescription = null)
+        {
+            // if there is a new base cost
+            if (newBaseCost.HasValue && newBaseCost < 0)
+            {
+               throw new InvalidTreatmentCostException();
+            }
+
+            if (newDescription is not null && !DescriptionPattern().IsMatch(newDescription))
+            {
+                throw new InvalidTreatmentDescriptionException("Description contains invalid characters or length.");
+            }
+
+            BaseCost = newBaseCost ?? BaseCost;
+            Description = newDescription ?? Description;
+        }
+
+
     }
 }
