11ba539f0fe67c06e8fb12faa06f64bb58cb47cb
Kimblag <kimberly.blandong@gmail.com>
2026-01-15 20:43:19 -0300

    refactor(domain): introduce AggregateRoot base class
    - Consolidates common aggregate behavior for Specialty and future aggregates.
    - Prepares foundation for UnitOfWork and domain tracking.

---
+++ src/DentalSystem.Domain/Common/AggregateRoot.cs
@@ -0,0 +1,12 @@
+﻿namespace DentalSystem.Domain.Common
+{
+    public abstract class AggregateRoot
+    {
+        public int Version { get; private set; } = 0;
+
+        protected void MarkAsModified()
+        {
+            Version++;
+        }
+    }
+}
