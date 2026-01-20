41babea30c119d27975761fd0c560a88e9d433f7
Kimblag <kimberly.blandong@gmail.com>
2026-01-20 11:07:07 -0300

    feat(infra): setup DbContextFactory and run initial specialty migration

---
+++ src/DentalSystem.Infrastructure/Migrations/20260120122704_InitialCreate_Specialty.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DentalSystem.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate_Specialty : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Specialty",
+                columns: table => new
+                {
+                    SpecialtyId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(500)", maxLength: 500, nullable: true),
+                    IsActive = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Specialty", x => x.SpecialtyId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Treatment",
+                columns: table => new
+                {
+                    TreatmentId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(500)", maxLength: 500, nullable: true),
+                    BaseCost = table.Column<decimal>(type: "decimal(10,2)", precision: 10, scale: 2, nullable: false),
+                    IsActive = table.Column<bool>(type: "bit", nullable: false),
+                    SpecialtyId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Treatment", x => x.TreatmentId);
+                    table.ForeignKey(
+                        name: "FK_Treatment_Specialty_SpecialtyId",
+                        column: x => x.SpecialtyId,
+                        principalTable: "Specialty",
+                        principalColumn: "SpecialtyId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Specialty_Name",
+                table: "Specialty",
+                column: "Name",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Treatment_SpecialtyId",
+                table: "Treatment",
+                column: "SpecialtyId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Treatment");
+
+            migrationBuilder.DropTable(
+                name: "Specialty");
+        }
+    }
+}
---
+++ src/DentalSystem.Infrastructure/Persistence/DentalSystemDbContextFactory.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+namespace DentalSystem.Infrastructure.Persistence
+{
+    public  class DentalSystemDbContextFactory : IDesignTimeDbContextFactory<DentalSystemDbContext>
+    {
+        public DentalSystemDbContext CreateDbContext(string[] args)
+        {
+            // Build configuration to get connection string
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            var builder = new DbContextOptionsBuilder<DentalSystemDbContext>();
+
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+
+            builder.UseSqlServer(connectionString);
+
+            return new DentalSystemDbContext(builder.Options);
+        }
+    }
+}
