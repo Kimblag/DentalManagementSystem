6dd41a49830fa26d044df42af1c2c81d639b44d5
Kimblag <kimberly.blandong@gmail.com>
2026-03-29 17:00:24 -0300

    feat(Application): Add create specialty use case.

---
+++ src/DentalSystem.Application/UseCases/Specialties/CreateSpecialty.cs
@@ -0,0 +1,36 @@
+﻿using DentalSystem.Application.DTOs.Specialties.Inputs;
+using DentalSystem.Application.DTOs.Specialties.Outputs;
+using DentalSystem.Application.Exceptions;
+using DentalSystem.Application.Interfaces;
+using DentalSystem.Application.Interfaces.Repositories;
+using DentalSystem.Application.Mappers.Specialties;
+using DentalSystem.Domain.Aggregates.Specialty;
+using DentalSystem.Domain.ValueObjects;
+
+namespace DentalSystem.Application.UseCases.Specialties
+{
+    public sealed class CreateSpecialty(ISpecialtyRepository repository, IUnitOfWork unitOfWork)
+    {
+        private readonly ISpecialtyRepository _repository = repository;
+        private readonly IUnitOfWork _unitOfWork = unitOfWork;
+
+        public async Task<SpecialtyResponse> HandleAsync(CreateSpecialtyRequest request)
+        {
+
+            // check if specialty already exists
+            bool nameExists = await _repository.ExistsByNameAsync(request.Name);
+            if (nameExists) throw new ApplicationConflictException($"A specialty with the name {request.Name} already exists");
+
+            // construir la specialty
+            // VOs
+            Name name = new(request.Name);
+            Specialty specialty = new(name, request.Description);
+
+            // persistir
+            await _repository.AddAsync(specialty);
+            await _unitOfWork.SaveChangesAsync();
+
+            return specialty.ToDto();
+        }
+    }
+}
